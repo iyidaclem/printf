aa6467ba0e5f73c1855b644209ee0cfe9b108a2d
Mary Oloyede <maryoloyedeayo@gmail.com>
2022-10-17 05:16:03 -0700

    main header

--- main.h
+++ main.h
@@ -1,6 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char op;
+	int (*f)(va_list);
+} op_t;
 int _putchar(char);
 int _printf(const char *format, ...);
 int s_handler(char *);
